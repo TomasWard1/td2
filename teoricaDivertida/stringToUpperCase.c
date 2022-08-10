dfa27234975051ff12a07790a3d1409b3ad96eb6
Tomas Ward <tomas@familiaward.com.ar>
2022-08-10 15:55:57 -0300

    strToUppercase hecho

--- teoricaDivertida/stringToUpperCase.c
+++ teoricaDivertida/stringToUpperCase.c
@@ -1,16 +1,25 @@
-#define _BSD_SOURCE 1
+#define _DEFAULT_SOURCE 1
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void stringToUpperCase(char* s) {
-    //usando la tabla ascii sin considerar caracteres raros.
-    // ?????????????????
+    for (int i = 0; s[i] != '\0'; i++)
+    {
+        int letra = s[i]; //lo interpretamos como int usando el ASCII table
+        if (letra >= 97) {
+            //esta en minuscula
+            int nuevaLetra = letra - 32; //lo cambiamos a mayuscula
+            s[i] = nuevaLetra;
+        }
+
+    }
+    
 }
 
 int main() {
 
-    char* a = "Hola Carola";
+    char* a = "Hola Carolaz";
     char* b = strdup(a);
 
     stringToUpperCase(b);
