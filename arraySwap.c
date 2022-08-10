c45443c8bea2aec272113ba248932fdabd566c83
Tomas Ward <35110844+TomasWard1@users.noreply.github.com>
2022-08-10 08:07:56 -0300

    Add files via upload

---
+++ arraySwap.c
@@ -0,0 +1,17 @@
+#define _BSD_SOURCE 1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void arraySwap(int* a, int size) {
+    // ????????????????????
+}
+
+int main() {
+
+    int a[9] = {1,2,3,4,5,6,7,8,9};
+
+    arraySwap(a,9);
+
+    return 0;
+}
